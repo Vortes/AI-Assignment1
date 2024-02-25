01b93817042608833c102bdb9af66e89570c77dc
Alan <zeyu.weng@gmail.com>
2024-02-25 17:05:15 -0500

    node expansion

--- adaptive.py
+++ adaptive.py
@@ -108,6 +108,57 @@ def heuristic(a: Cell, b: Cell) -> int:
     return abs(a.x - b.x) + abs(a.y - b.y)
 
 
+def adaptive_a_star_search_experiment(maze, start, goal, use_updated_heuristic=False):
+    # I need to make sure that the algorithm reruns to show that its choosing more efficient paths
+    open_set = PriorityQueue()
+    start_coords = (start.x, start.y)
+    goal_coords = (goal.x, goal.y)
+    # heap has structure of (priority, coords)
+    open_set.put((0, start_coords))
+    came_from = {}
+    # I need to create an explicit closed list.
+    # rn I'm using cost_so_far to retrace my steps
+    g_cost = {}
+    # came_from is a dictionary that maps a node to the parent node
+    came_from[start_coords] = None
+    g_cost[start_coords] = 0
+
+    expanded_nodes = 0
+
+    while not open_set.empty():
+        current_priority, current_coords = open_set.get()
+        # Increment counter each time a node is expanded
+        expanded_nodes += 1
+        # unpacking the tuple current_coords into maze
+        current = maze.get(*current_coords)
+        if current_coords == goal_coords:
+            # print("Goal reached!")
+            # print(g_cost)
+            if not use_updated_heuristic:
+                print("in here")
+                update_all_heuristics(came_from, g_cost, goal, maze)
+            break
+
+        """
+        1. Iterate through the neighbors of the current node.
+        2. For each neighbor, calculate its coordinates.
+        3. Determine the new path cost to reach this neighbor (current cost to reach the current node + 1)
+        4. if the neighbor has not been visited or the new cost is lower, update the open list to include the neighbor
+        """
+        for next in maze.neighbors(current):
+            next_coords = (next.x, next.y)
+            new_cost = g_cost[current_coords] + 1
+            if next_coords not in g_cost or new_cost < g_cost[next_coords]:
+                g_cost[next_coords] = new_cost
+                priority = new_cost + \
+                    (next.h if use_updated_heuristic else heuristic(goal, next))
+                next.update_heuristic(heuristic(goal, next))
+                open_set.put((priority, next_coords))
+                came_from[next_coords] = current_coords
+
+    return expanded_nodes
+
+
 def adaptive_a_star_search(maze, start, goal, use_updated_heuristic=False):
     # I need to make sure that the algorithm reruns to show that its choosing more efficient paths
     open_set = PriorityQueue()
@@ -162,9 +213,9 @@ def update_all_heuristics(came_from, g_cost, goal, maze):
         node = maze.get(*coords)
         # print("node: ", node.h)
         # Update heuristic based on most recent search
-        # print("node.h value before update: ", node.h)
+        print("node.h value before update: ", node.h)
         node.h = g_goal - g_cost[coords]
-        # print("node.h value after update: ", node.h)
+        print("node.h value after update: ", node.h)
 
 
 def reconstruct_path(came_from, start, goal, maze):
@@ -194,6 +245,22 @@ def draw_path(screen, path, color=(0, 255, 0), opacity=255):
     for node in path:
         # Blit the temporary surface onto the screen at the node's position
         screen.blit(temp_surface, (node.rect.x, node.rect.y))
+
+
+def adaptive_a_star_maze_experiment(screen, num_mazes):
+    expanded_cells = []
+    for i in range(int(num_mazes)):
+        print("Testing maze", i+1, "out of", num_mazes, "!")
+        maze = Maze(WINSIZE)
+        maze.generate(screen, True)
+        start = maze.get(1, 1)
+        goal = maze.get(maze.w - 2, maze.h - 2)
+        expanded_nodes = adaptive_a_star_search_experiment(
+            maze, start, goal)
+
+        expanded_cells.append(expanded_nodes)
+
+    print("sum of expanded cells", sum(expanded_cells))
 
 
 def draw_maze(screen):
@@ -229,14 +296,22 @@ def main():
     clock = pygame.time.Clock()
     draw_maze(screen)
 
-    done = False
-    while not done:
-        for e in pygame.event.get():
-            if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
-                done = True
+    num_mazes = input("How many mazes would you like to generate? ")
+    # print("OK! Generating", command, "mazes...")
+    # maze_list = generate_maze_list(screen, command)
+    # print("Finished!")
+    command = input("What would you like to do? ")
+    if command == "1":
+        adaptive_a_star_maze_experiment(screen, num_mazes)
+    else:
+        done = False
+        while not done:
+            for e in pygame.event.get():
+                if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                    done = True
 
-        pygame.display.update()
-        clock.tick(60)
+            pygame.display.update()
+            clock.tick(60)
 
 
 if __name__ == '__main__':
